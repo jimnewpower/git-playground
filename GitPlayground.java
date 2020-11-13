c70ed721719f4a1237f982d81007034174e9b344
Jim Newpower <jimnewpower@gmail.com>
2020-11-13 10:03:43 -0700

    branch-1: Add branch1()

--- GitPlayground.java
+++ GitPlayground.java
@@ -2,4 +2,8 @@ public class GitPlayground {
     public static void main(String[] args) {
       System.out.println("Hello Git");
     }
+
+    public void branch1() {
+      // method added in branch-1
+    }
 }
